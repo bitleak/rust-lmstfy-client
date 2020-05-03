17f57fb2e7b9aadc719626692984b6c03c07f5e2
vjoke <vjoke@foxmail.com>
2020-05-03 18:22:55 +0800

    ADD: doc

--- src/api.rs
+++ src/api.rs
@@ -1,41 +1,58 @@
-use crate::errors::{APIError, ErrType};
-use reqwest::{header::HeaderValue, Client as HTTPClient, StatusCode};
-use serde::Deserialize;
 use std::path::Path;
-
+use serde::Deserialize;
+use reqwest::{header::HeaderValue, Client as HTTPClient, StatusCode};
+use crate::errors::{APIError, ErrType};
+/// Max read timeout value in second
 const MAX_READ_TIMEOUT: u32 = 600;
+/// Max batch consume size
 const MAX_BATCH_CONSUME_SIZE: u32 = 100;
 
 type Result<T> = std::result::Result<T, APIError>;
 
 /// The client for lmstfy
 pub struct Client {
+    /// The namespace for the client
     pub namespace: String,
+    /// The access token of api
     pub token: String,
+    /// The target host for the server
     pub host: String,
+    /// The target port for the server
     pub port: u32,
     /// Retry when `publish` failed
     pub retry: u32,
-    /// Unit: millisecond
-    pub back_off: u32,
-    /// http client
+    /// backoff time when retrying
+    pub backoff: u32,
+    /// http client which is used to communicate with server
     pub http_client: HTTPClient,
 }
 
+/// The normal response for publishing tasks 
 #[derive(Deserialize)]
 pub struct PublishResponse {
+    /// The job id corresponding to the published task
     job_id: String,
 }
 
-/// The client itself
+/// The client implementation
 impl Client {
+    /// Returns a client with the given parameters
+    /// 
+    /// # Arguments
+    /// 
+    /// * `namespace` - A str that holds the namespace of the client
+    /// * `token` - A str that holds the token for api request
+    /// * `host` - A str that holds the target server
+    /// * `port` - A u32 value that holds the target port
+    /// * `retry` - A u32 value that holds the retry count
+    /// * `backoff` - A u32 value that holds the backoff value
     pub fn new(
         namespace: &str,
         token: &str,
         host: &str,
         port: u32,
         retry: u32,
-        back_off: u32,
+        backoff: u32,
     ) -> Self {
         Client {
             namespace: namespace.to_string(),
@@ -43,12 +60,21 @@ impl Client {
             host: host.to_string(),
             port: port,
             retry: retry,
-            back_off: back_off,
+            backoff: backoff,
             http_client: HTTPClient::new(),
         }
     }
 
-    /// Innner function to publish message
+    /// Innner function to publish task
+    /// 
+    /// # Arguments
+    /// 
+    /// * `queue` - A string that holds the queue for the task
+    /// * `ack_job_id` - A string that holds the job id about to acknowledged
+    /// * `data` - A vector of byte that holds the content of task
+    /// * `ttl` - A u32 value that holds the time-to-live value
+    /// * `tries` - A u32 value that holds the maximize retry count
+    /// * `delay` - A u32 value that holds the delay value in second
     async fn do_publish(
         &self,
         queue: String,
@@ -106,6 +132,16 @@ impl Client {
 
 /// The API implementation for lmstfy
 impl Client {
+    // Publish task to the server
+    /// 
+    /// # Arguments
+    /// 
+    /// * `queue` - A String that holds the queue for the task
+    /// * `ack_job_id` - A string that holds the job id about to acknowledged
+    /// * `data` - A vector of byte that holds the content of task
+    /// * `ttl` - A u32 value that holds the time-to-live value
+    /// * `tries` - A u32 value that holds the maximize retry count
+    /// * `delay` - A u32 value that holds the delay value in second
     pub async fn publish(
         &self,
         queue: String,
@@ -128,16 +164,16 @@ impl Client {
             });
         }
 
-        let (job_id, request_id, status_code) = ret.unwrap();
-        if status_code.is_success() {
-            return Ok((job_id, request_id));
+        if !status_code.is_success() {
+            return Err(APIError {
+                err_type: ErrType::ResponseErr,
+                reason: status_code.canonical_reason().unwrap().to_string(),
+                job_id: job_id,
+                request_id: request_id,
+            }); 
         }
 
-        return Err(APIError {
-            err_type: ErrType::ResponseErr,
-            reason: status_code.canonical_reason().unwrap().to_string(),
-            job_id: job_id,
-            request_id: request_id,
-        });
+        let (job_id, request_id, status_code) = ret.unwrap();
+        return Ok((job_id, request_id));
     }
 }
